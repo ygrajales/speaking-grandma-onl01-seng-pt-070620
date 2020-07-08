bd06bde36dca89b3034050d037c89d8602872605
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-08 01:57:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(phrase="Hi, Grandma!")
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
