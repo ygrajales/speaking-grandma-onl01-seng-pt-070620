dd147da458f2b025a79495a2e3fe43527624a648
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-08 01:52:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase="Hi, Grandma!")
-  if phrase != phrase.upcase
+  if phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
-    puts 
+    puts "NO, NOT SINCE 1938!"
+  end
